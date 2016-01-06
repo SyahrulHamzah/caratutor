4807fef7bfc6e1f80aa5dbd5183002fc2a296d9a
SyahrulHamzah <bajingvskorak@gmail.com>
2016-01-06 14:32:08 +0800

    Delete Homejv.js

--- Homejv.js
+++
@@ -1,2 +0,0 @@
-var imgDefer=document.getElementsByTagName("img");for(var i=0;i<imgDefer.length;i++){if(imgDefer[i].getAttribute("data-src")){imgDefer[i].setAttribute("src",imgDefer[i].getAttribute("data-src"))}};
-function resizeThumb(e,t,g){for(var g=document.getElementById(e),m=g.getElementsByTagName("img"),r=0;r<m.length;r++)m[r].src=m[r].src.replace(/\/s72\-c/,"/s"+t),m[r].width=t,m[r].height=t}resizeThumb("Blog1",240,90);
